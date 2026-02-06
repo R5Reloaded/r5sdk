074a1b958f4a76335f6fd12a6ac10c6a30e88b9f
O-Robotic <O-Robotic@users.noreply.github.com>
2026-02-06 12:16:25 +0000

    Tier1: CLZSS class constification

--- src/public/tier1/lzss.h
+++ src/public/tier1/lzss.h
@@ -30,12 +30,12 @@ class CLZSS
 {
 public:
 	unsigned char*	Compress( unsigned char *pInput, int inputlen, unsigned int *pOutputSize );
-	unsigned char*	CompressNoAlloc( unsigned char *pInput, int inputlen, unsigned char *pOutput, unsigned int *pOutputSize );
-	unsigned int	Uncompress( unsigned char *pInput, unsigned char *pOutput );
+	unsigned char*	CompressNoAlloc( const unsigned char *pInput, int inputlen, unsigned char *pOutput, unsigned int *pOutputSize );
+	unsigned int	Uncompress( unsigned char *pInput, unsigned char *pOutput ) const;
 	//unsigned int	Uncompress( unsigned char *pInput, CUtlBuffer &buf );
-	unsigned int	SafeUncompress( unsigned char *pInput, unsigned char *pOutput, unsigned int unBufSize );
-	bool			IsCompressed( unsigned char *pInput );
-	unsigned int	GetActualSize( unsigned char *pInput );
+	unsigned int	SafeUncompress( const unsigned char *pInput, unsigned char *pOutput, unsigned int unBufSize ) const;
+	bool			IsCompressed( const unsigned char *pInput ) const;
+	unsigned int	GetActualSize( const unsigned char *pInput ) const;
 
 	// windowsize must be a power of two.
 	FORCEINLINE CLZSS( int nWindowSize = DEFAULT_LZSS_WINDOW_SIZE );
@@ -44,7 +44,7 @@ private:
 	// expected to be sixteen bytes
 	struct lzss_node_t
 	{
-		unsigned char	*pData;
+		const unsigned char	*pData;
 		lzss_node_t		*pPrev;
 		lzss_node_t		*pNext;
 		char			empty[4];
@@ -56,7 +56,7 @@ private:
 		lzss_node_t *pEnd;
 	};
 
-	void			BuildHash( unsigned char *pData );
+	void			BuildHash( const unsigned char *pData );
 	lzss_list_t		*m_pHashTable;	
 	lzss_node_t		*m_pHashTarget;
 	int             m_nWindowSize;
