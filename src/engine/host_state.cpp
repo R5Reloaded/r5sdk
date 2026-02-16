2a69abd494a3e08505b8e8b4bf6055c0a3747d66
O-Robotic <98644082+O-Robotic@users.noreply.github.com>
2026-02-16 13:37:29 +0000

    Pylon: Normalise all ip addresses used in the auth process.

--- src/engine/host_state.cpp
+++ src/engine/host_state.cpp
@@ -102,7 +102,7 @@ static void HostState_KeepAlive()
 		{
 			string errorMsg;
 			string hostToken;
-			string hostIp;
+			CNetAdr hostIp;
 
 			const bool result = g_MasterServer.PostServerHost(errorMsg, hostToken, hostIp, gameServer);
 
@@ -128,8 +128,7 @@ static void HostState_KeepAlive()
 						}
 					}
 
-					if (hostIp.length() != 0)
-						g_ServerHostManager.SetHostIP(hostIp);
+					g_ServerHostManager.SetHostIP(hostIp);
 
 				}, 0);
 		}
