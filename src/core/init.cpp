8f5df9e18cf5a33e45eec56ca72ba9fb87cd7ec4
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2026-06-24 17:35:36 +0200

    Core: break out as soon as we found the primary GPU
    According to microsoft documentation @ https://learn.microsoft.com/en-us/windows/win32/api/wingdi/ns-wingdi-display_devicea, only one device can have this flag set. So break out as soon as we found it.

--- src/core/init.cpp
+++ src/core/init.cpp
@@ -417,6 +417,7 @@ void QuerySystemInfo()
 		if (dd.StateFlags & DISPLAY_DEVICE_PRIMARY_DEVICE) // Only log the primary device.
 		{
 			Msg(eDLL_T::NONE, "%-25s: '%s'\n", "GPU model identifier", dd.DeviceString);
+			break;
 		}
 	}
 #endif // !DEDICATED
